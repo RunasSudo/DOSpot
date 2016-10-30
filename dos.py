7cf62f99a0d4f689feffc1bb9a01089aea838552
RunasSudo <runassudo@yingtongli.me>
2016-10-30 20:39:57 +1030

    Improve ALL THE THINGS!
    - Better architecture for handling errors
    - Case insensitivity
    - Check for invalid drives
    - Handle relative paths properly
    - Use a dict like proper Python coders
    - Sort dir files
    - Implement cd

--- dos.py
+++ dos.py
@@ -22,6 +22,11 @@
 
 import threading
 
+class DOSException(Exception):
+	def __init__(self, message):
+		self.message = message
+		super().__init__(message)
+
 class DOSpot(threading.Thread):
 	def __init__(self, fin, fout, islocal=False):
 		super().__init__(daemon=True)
@@ -81,14 +86,128 @@ class DOSpot(threading.Thread):
 		self.print('MS-DOS Version 4.01')
 		
 		def pwd(drv=self.currdrv, dir_=self.currdir):
-			return '{}:\\{}'.format(drv, '\\'.join(dir_))
+			return '{}:\\{}'.format(drv, '\\'.join(dir_)).upper()
 		
 		def traverse(path, tree=self.roottree):
 			if len(path) == 0:
 				return tree
-			if path[0] in tree:
-				return traverse(path[1:], tree[path[0]])
+			if path[0].upper() in tree:
+				return traverse(path[1:], tree[path[0].upper()])
 			return False
+		
+		def checkpath(path, tree=self.roottree):
+			if traverse(path, tree) is False:
+				raise DOSException('File not found')
+			
+			return True
+		
+		def resolvepath(path):
+			dir_ = []
+			
+			if len(path[0]) == 2 and path[0][1] == ':':
+				# absolute path
+				if path[0][0] == self.currdrv:
+					path = path[1:]
+				else:
+					raise DOSException('Invalid drive specification')
+			else:
+				# relative path
+				dir_.extend(self.currdir)
+			
+			for bit in path:
+				if bit == '.':
+					pass
+				elif bit == '..':
+					if len(dir_) > 0:
+						dir_.pop()
+					else:
+						raise DOSException('Invalid directory')
+				else:
+					dir_.append(bit)
+			
+			return dir_
+		
+		
+		def cmd_cd(s, args, cmd):
+			dir_ = resolvepath(args[1].split('\\'))
+			checkexists(dir_)
+			currdir = dir_
+		
+		def cmd_del(s, args, cmd):
+			dir_ = resolvepath(args[1].split('\\'))
+			checkexists(dir_)
+			
+			target = traverse(dir_)
+			if type(target) == dict:
+				self.print('All files in directory will be deleted!')
+				resp = self.input('Are you sure (Y/N)?').strip()
+				if resp.lower() == 'y':
+					raise DOSException('Access denied')
+			else:
+				raise DOSException('Access denied')
+		
+		def cmd_dir(s, args, cmd):
+			try:
+				dir_ = resolvepath(args[1].split('\\')) if len(args) > 1 else self.currdir
+			except DOSException as ex:
+				if ex.message == 'Invalid directory':
+					self.print()
+					self.print(' Volume in drive {} is ROOTDISK'.format(self.currdrv))
+					self.print(' Volume Serial Number is 2958-0B1B')
+					self.print(' Directory of  {}'.format(pwd(self.currdrv, self.currdir)))
+					self.print()
+					raise ex
+				else:
+					raise ex
+			
+			self.print()
+			self.print(' Volume in drive {} is ROOTDISK'.format(self.currdrv))
+			self.print(' Volume Serial Number is 2958-0B1B')
+			
+			if traverse(dir_) is not False:
+				self.print(' Directory of  {}'.format(pwd(self.currdrv, dir_)))
+				self.print()
+				
+				num = 0
+				if len(dir_) > 0:
+					self.print('{:<8}     <DIR>     04-07-89  12:00a'.format('.'))
+					self.print('{:<8}     <DIR>     04-07-89  12:00a'.format('..'))
+					num += 2
+				for item, value in sorted(sorted(traverse(dir_).items(), key=lambda x: x[0]), key=lambda x: type(x[1]) == dict):
+					if type(value) == dict:
+						self.print('{:<8}     <DIR>     04-07-89  12:00a'.format(item))
+					else:
+						self.print('{:<8} {:<3} {:>9} 04-07-89  12:00a'.format(*item.split('.'), value))
+					num += 1
+				
+				self.print('{:>9} File(s) {:>10} bytes free'.format(num, '31141889'))
+			else:
+				if traverse(dir_[:-1]) is False:
+					raise DOSException('Path not found')
+				else:
+					self.print(' Directory of  {}'.format(pwd(self.currdrv, dir_[:-1])))
+					self.print()
+					raise DOSException('File not found')
+		
+		def cmd_echo(s, args, cmd):
+			arg = s[5:]
+			if arg.lower() == 'on':
+				self.c_echo = True
+				return
+			if arg.lower() == 'off':
+				self.c_echo = False
+				return
+			if len(arg) > 0 or s.startswith('echo.'):
+				self.print(arg)
+				return
+			self.print('ECHO is {}'.format('on' if self.c_echo else 'off'))
+		
+		def cmd_ver(s, args, cmd):
+			self.print()
+			self.print('MS-DOS Version 4.01')
+		
+		
+		cmds = {'cd': cmd_cd, 'del': cmd_del, 'dir': cmd_dir, 'echo': cmd_echo, 'ver': cmd_ver}
 		
 		while True:
 			s = self.input('{}>'.format(pwd()) if self.c_echo else '').strip()
@@ -97,55 +216,17 @@ class DOSpot(threading.Thread):
 				args = s.split()
 				cmd = args[0].lower()
 				
-				if cmd == 'echo' or s.startswith('echo.'):
-					arg = s[5:]
-					if arg.lower() == 'on':
-						self.c_echo = True
-					elif arg.lower() == 'off':
-						self.c_echo = False
-					elif len(arg) > 0 or s.startswith('echo.'):
-						self.print(arg)
+				try:
+					if cmd in cmds:
+						cmds[cmd](s, args, cmd)
+					elif s.startswith('cd.'):
+						cmds['cd']('cd ' + s[3:], ['cd', args[0][2:]] + args[1:])
+					elif s.startswith('echo.'):
+						cmds['echo'](s, args, cmd)
 					else:
-						self.print('ECHO is {}'.format('on' if self.c_echo else 'off'))
-				elif cmd == 'dir':
-					dir_ = self.currdir + ([] if len(args) == 1 else [args[1]])
-					
-					self.print()
-					self.print(' Volume in drive {} is ROOTDISK'.format(self.currdrv))
-					self.print(' Volume Serial Number is 2958-0B1B')
-					self.print(' Directory of  {}'.format(pwd(self.currdrv, dir_)))
-					self.print()
-					
-					num = 0
-					if len(dir_) > 0:
-						self.print('{:<8}     <DIR>     04-07-89  12:00a'.format('.'))
-						self.print('{:<8}     <DIR>     04-07-89  12:00a'.format('..'))
-						num += 2
-					for item, value in traverse(dir_).items():
-						if type(value) == dict:
-							self.print('{:<8}     <DIR>     04-07-89  12:00a'.format(item))
-						else:
-							self.print('{:<8} {:<3} {:>9} 04-07-89  12:00a'.format(*item.split('.'), value))
-						num += 1
-					
-					self.print('{:>9} File(s) {:>10} bytes free'.format(num, '31141889'))
-				elif cmd == 'del':
-					target = traverse(self.currdir + [args[1]])
-					if target is not False:
-						if type(target) == dict:
-							self.print('All files in directory will be deleted!')
-							resp = self.input('Are you sure (Y/N)?').strip()
-							if resp.lower() == 'y':
-								self.print('Access denied')
-						else:
-							self.print('Access denied')
-					else:
-						self.print('File not found')
-				elif cmd == 'ver':
-					self.print()
-					self.print('MS-DOS Version 4.01')
-				else:
-					self.print('Bad command or file name')
+						self.print('Bad command or file name')
+				except DOSException as ex:
+					print(ex)
 				
 				if self.c_echo:
 					self.print()
